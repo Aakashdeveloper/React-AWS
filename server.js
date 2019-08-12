ff00f2265274226c6067a1475788253b8b6f7d38
aakashDeveloper <ahanda205@gmail.com>
2019-08-12 08:15:08 +0100

    a

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const app = express();
+
+// Angular DIST output folder
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Send all other requests to the Angular app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build/index.html'));
+})
+
+//Set Port
+const port = process.env.PORT || '3046';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Running on localhost:${port}`));
