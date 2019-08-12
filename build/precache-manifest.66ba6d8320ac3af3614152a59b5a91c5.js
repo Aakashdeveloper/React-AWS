011ef4d5951ec582caef6d50e6f2be9d89df96ee
aakashDeveloper <ahanda205@gmail.com>
2019-08-12 08:15:34 +0100

    a

---
+++ build/precache-manifest.66ba6d8320ac3af3614152a59b5a91c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff89b8f49ea2cd52049349614574f9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b216c25dda5615d7d69",
+    "url": "/static/css/main.e2bfd105.chunk.css"
+  },
+  {
+    "revision": "d22f04aa21b30375c636",
+    "url": "/static/js/2.e0845511.chunk.js"
+  },
+  {
+    "revision": "0b216c25dda5615d7d69",
+    "url": "/static/js/main.e854504e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
